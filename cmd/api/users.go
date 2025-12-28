a7a38c3628d8cb6dcfbdb267e009c4c724896814
ilx <iliakeshavarz23@gmail.com>
2025-12-28 10:22:31 +0330

    add user delete and get user inoramtions handlers

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -14,6 +14,7 @@ func (app *application) createUserHandler(w http.ResponseWriter, r *http.Request
 		LastName  string `json:"last_name"`
 		Email     string `json:"email"`
 		Password  string `json:"password"`
+		Role      string `json:"role"`
 	}
 
 	err := app.readJSON(w, r, &input)
@@ -83,7 +84,6 @@ func (app *application) updateUserHandler(w http.ResponseWriter, r *http.Request
 		FirstName string `json:"first_name"`
 		LastName  string `json:"last_name"`
 		Email     string `json:"email"`
-		Password  string `json:"password"`
 	}
 
 	err = app.readJSON(w, r, &input)
@@ -101,9 +101,11 @@ func (app *application) updateUserHandler(w http.ResponseWriter, r *http.Request
 	if input.Email != "" {
 		user.Email = input.Email
 	}
-	if input.Password != "" {
-		user.Password.Set(input.Password)
-	}
+
+	//TODO: adding a forget password functionalitty
+	// if input.Password != "" {
+	// 	user.Password.Set(input.Password)
+	// }
 
 	err = app.models.Users.Update(*user)
 	if err != nil {
@@ -117,6 +119,59 @@ func (app *application) updateUserHandler(w http.ResponseWriter, r *http.Request
 	}
 
 	err = app.writeJSON(w, r, http.StatusOK, jsFmt{"message": "user successfully updated"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
+
+func (app *application) deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(w, r)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	//TODO: adding passowrd check after adding user authentication
+	err = app.models.Users.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, models.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, r, http.StatusOK, jsFmt{"messeage": "user successfully deleted."}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
+
+func (app *application) userInformationHandler(w http.ResponseWriter, r *http.Request) {
+
+	id, err := app.readIDParam(w, r)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	user, err := app.models.Users.GetUser(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, models.ErrRecordNotFound):
+			app.noUserFound(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	//TODO: adding user authentication in order to keep other users from seeing another user informations is URGENT
+	err = app.writeJSON(w, r, http.StatusOK, jsFmt{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
