c9e41cdc7837db0760ca1409dc01a56cd4dadcbc
ilx <iliakeshavarz23@gmail.com>
2025-12-28 10:55:48 +0330

    add Restaurants DB to models

--- internal/models/models.go
+++ internal/models/models.go
@@ -6,17 +6,20 @@ import (
 )
 
 var (
-	ErrDuplicateEmail = errors.New("duplicate email address")
-	ErrRecordNotFound = errors.New("no record found")
-	ErrConflictEdit   = errors.New("conflict edit")
+	ErrDuplicateEmail          = errors.New("duplicate email address")
+	ErrRecordNotFound          = errors.New("no record found")
+	ErrConflictEdit            = errors.New("conflict edit")
+	ErrDuplicateRestaurantName = errors.New("duplicate restaurant name")
 )
 
 type Models struct {
-	Users *UserModel
+	Users       *UserModel
+	Restaurants *RestaurantModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Users: &UserModel{DB: db},
+		Users:       &UserModel{DB: db},
+		Restaurants: &RestaurantModel{DB: db},
 	}
 }
