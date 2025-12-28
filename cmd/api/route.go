f67d2eab35a7db252de6db2fee111506e7342391
ilx <iliakeshavarz23@gmail.com>
2025-12-28 10:54:28 +0330

    add DELETE /v1/users/:id, GET /v1/users/:id, POST /v1/restaurant

--- cmd/api/route.go
+++ cmd/api/route.go
@@ -14,9 +14,18 @@ func (app *application) route() http.Handler {
 		app.notFoundResponse(w, r)
 	})
 
+	router.MethodNotAllowed = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		app.errorResponse(w, r, http.StatusMethodNotAllowed, "Method not allowed for this reousrce")
+
+	})
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheck)
+	router.HandlerFunc(http.MethodGet, "/v1/users/:id", app.userInformationHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.createUserHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/users/:id", app.updateUserHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/users/:id", app.deleteUserHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/restaurant", app.restaurantCreateHandler)
 
 	return router
 }
