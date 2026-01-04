b852bcbd8d25ff3e9316cb5800e4214d2bad0534
ilx <iliakeshavarz23@gmail.com>
2026-01-04 10:36:43 +0330

    add a userContext type and not using built-In types in orer to avoid collisions and add two methods called getUserContext and setUserContext

---
+++ cmd/api/context.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/geekilx/restaurantAPI/internal/models"
+)
+
+type contextKey string
+
+const userContextKey = contextKey("user")
+
+func (app *application) getUserContext(r *http.Request) models.User {
+
+	user, ok := r.Context().Value(userContextKey).(models.User)
+	if !ok {
+		panic("missing user value in the request context")
+	}
+
+	return user
+
+}
+
+func (app *application) setUserContext(w http.ResponseWriter, r *http.Request, user *models.User) *http.Request {
+	ctx := context.WithValue(r.Context(), userContextKey, user)
+
+	return r.WithContext(ctx)
+
+}
